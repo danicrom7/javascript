bf59ecb88db26fc1fae422a92bde1fb640f6ca12
danicrom7 <daniel.garcia.lindo@tajamar365.com>
2017-05-19 21:40:51 +0200

    19 de mayo objetos

---
+++ objetos/js/obj1.js
@@ -0,0 +1,19 @@
+//objetos
+let oSample1 = {};
+let oSample2 = new Object();
+
+console.log(typeof oSample1);
+console.log(typeof oSample2);
+
+console.log(oSample1)
+console.log(oSample1)
+//le das las propiedas que quieras, no tienes porque crear los objetos con dichas propiedades.
+oSample1.nombre = "";
+oSample2.titulo = "";
+oSample2.Olibro = {};
+
+oSample2.Olibro.titulo = "";
+
+//array
+let aSample = [];
+let aSample2 = new Array[];
---
+++ objetos/js/obj2.js
@@ -0,0 +1,17 @@
+let flight = {Number:815,
+    Departure:"clave IATA",
+    Time:new Date(2004,09,22, 14,55),
+    city:"Sydney",
+    arrival:"LAX",
+    llegada:"10:42",
+    destino:"Los Angeles",
+
+    mostrar : () =>{
+        console.log(flight);
+
+    }
+              }
+
+              flight.mostrar()
+
+
