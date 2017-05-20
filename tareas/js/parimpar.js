c177c9776e060c730000bbc824bfb22a87c6a227
danicrom7 <daniel.garcia.lindo@tajamar365.com>
2017-05-20 23:28:23 +0200

    prueba tarea 1 20 de mayo

---
+++ tareas/js/parimpar.js
@@ -0,0 +1,16 @@
+(function suerte (num1){
+   var num1 = prompt("Introduce un numero entero");
+
+   var result = suerte(num1);
+   console.log("El numero "+num1+" es "+result);
+
+
+    if (num % 2 == 0) {
+        return "par";
+        
+    } else {
+        return "impar";
+        
+    }
+
+}); 
